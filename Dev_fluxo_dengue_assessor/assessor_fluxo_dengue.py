51198e359f1fa0a6fe903c7ebced3fda64146713
KenzoMasuda <108629795+KenzoMasuda@users.noreply.github.com>
2023-04-28 14:54:51 -0300

    update fluxo_dengue_assessor
    Correções e mudanças de acesso a credenciais

--- Dev_fluxo_dengue_assessor/assessor_fluxo_dengue.py
+++ Dev_fluxo_dengue_assessor/assessor_fluxo_dengue.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Mar 21 13:01:40 2023
 
-@author: INFO
+@author: KENZO
 """
 def _agravos_dengue_assessor():
     from selenium import webdriver
@@ -23,22 +23,31 @@ def _agravos_dengue_assessor():
             else:
                 os.remove(Path(path, file))
                 
-    def download_agravos(agravos):
+    def download_agravos(agravos):    
         for agravo in agravos:
-            driver.switch_to.frame('frame_div_110556')
-            time.sleep(2)
-            Select(driver.find_element(By.XPATH, '//*[@id="vSDCONTROLEAGRAVOSSITUACAO"]')).select_by_visible_text("Todos")
-        #driver.find_element('xpath', '//*[@id="vCIDAJAX"]').send_keys("A90")
-            driver.find_element('id', 'vCIDAJAX').send_keys(agravo)
-            time.sleep(2)
-            driver.find_element('xpath', '/html/body/div[3]').click()
-            time.sleep(2)
-            driver.find_element('id', 'SEARCHBUTTON').click()
-            time.sleep(2)
-            driver.find_element('xpath', '//*[@id="EXPORT"]').click()
-            time.sleep(2)
-            driver.find_element('id', 'CLEARBUTTON').click()   
-            driver.switch_to.default_content()
+            again = True
+            while again:
+                try:
+                    driver.switch_to.frame('frame_div_110556')
+                    time.sleep(2)
+                    Select(driver.find_element(By.XPATH, '//*[@id="vSDCONTROLEAGRAVOSSITUACAO"]')).select_by_visible_text("Todos")
+                #driver.find_element('xpath', '//*[@id="vCIDAJAX"]').send_keys("A90")
+                    driver.find_element('id', 'vCIDAJAX').click()
+                    driver.find_element('id', 'vCIDAJAX').send_keys(agravo)
+                    time.sleep(2)
+                    #driver.find_element('xpath', '/html/body/div[3]').click()
+                    driver.find_element('xpath', '//*[@id="ui-active-menuitem"]/div/div[2]').click()
+                    time.sleep(2)
+                    driver.find_element('id', 'SEARCHBUTTON').click()
+                    time.sleep(2)
+                    driver.find_element('xpath', '//*[@id="EXPORT"]').click()
+                    time.sleep(2)
+                    driver.find_element('id', 'CLEARBUTTON').click()   
+                    driver.switch_to.default_content()
+                    again = False
+                except:
+                    print('aqui')
+                    again = True
             
     def concat_agravos_dengue_assessor(path):
         list_agravos = glob(path + "\*.xls")
@@ -53,8 +62,8 @@ def _agravos_dengue_assessor():
     
     CID_AGRAVOS = ["A90", "A91"]
     
-    user = "PHFB"
-    password = "120499"
+    user = os.environ.get('login_assessor')
+    password = os.environ.get('pass_assessor')
     
     diretorio_atual = os.getcwd()
     default_download = os.path.join(diretorio_atual, "exportacoes_teste")
@@ -69,14 +78,13 @@ def _agravos_dengue_assessor():
     
     driver = webdriver.Chrome('chromedriver.exe', chrome_options=chromeOptions)
     
-    
     #----------------------------------------------------------------
     #1°Login + MENU - Atendimento - Controle de Agravos
     driver.get('https://s56.asp.srv.br/saude.pm.barretos.sp/login')
     
-    driver.find_element('xpath', '//*[@id="vASPUSUARIOLOGINLOGIN"]').send_keys(user)
-    time.sleep(1)
     driver.find_element('xpath', '//*[@id="vASPUSUARIOLOGINSENHA"]').send_keys(password)
+    time.sleep(1)
+    driver.find_element('xpath', '//*[@id="vASPUSUARIOLOGINLOGIN"]').send_keys(user)
     
     driver.find_element('id', 'IMGASPLOGIN').click()
     time.sleep(1)
@@ -84,86 +92,21 @@ def _agravos_dengue_assessor():
     time.sleep(1)
     driver.find_element('xpath', '//*[@id="ext-gen93"]').click() #click atendimento
     time.sleep(1)
-    driver.find_element('xpath', '//*[@id="ext-gen148"]').click() #click Controle de Agravos
+    driver.find_element('xpath', '//*[@id="ext-gen150"]').click() #click Controle de Agravos
     time.sleep(1)
-    
-    #driver.switch_to.frame('frame_div_110556') #selecionar frame 'Manutenção Controle de agravos" 
-    
     
     remove_old_files(default_download, ".xls")
     download_agravos(CID_AGRAVOS)    
     concat_agravos_dengue_assessor(default_download)
     os.startfile("exportacoes_teste", "open")
 
-<<<<<<< Updated upstream
+    
 if __name__ == "__main__":
     _agravos_dengue_assessor()
     print("Agravos de dengue do assessor concatenados na pasta exportacoes")
-=======
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-import os
-import time
 
 
-CID_AGRAVOS = ["A90", "A91"]
-
-user = "PHFB"
-password = "120499"
-
-diretorio_atual = os.getcwd()
-caminho_pasta_download = os.path.join(diretorio_atual, "exportacoes_teste")
-if not os.path.isdir(caminho_pasta_download):
-    os.mkdir(caminho_pasta_download)
-report_download_path = caminho_pasta_download
-
-chromeOptions = webdriver.ChromeOptions()    
-prefs = {"download.default_directory" : report_download_path}
-chromeOptions.add_experimental_option("prefs", prefs)
-chromeOptions.add_argument('--ignore-ssl-errors=yes')
-chromeOptions.add_argument('--ignore-certificate-errors')
-
-driver = webdriver.Chrome('chromedriver.exe', chrome_options=chromeOptions)
 
 
-#----------------------------------------------------------------
-#1°Login + MENU - Atendimento - Controle de Agravos
-driver.get('https://s56.asp.srv.br/saude.pm.barretos.sp/login')
-
-driver.find_element('id', 'vASPUSUARIOLOGINLOGIN').send_keys(user)
-driver.find_element('id', 'vASPUSUARIOLOGINSENHA').send_keys(password)
-
-driver.find_element('id', 'IMGASPLOGIN').click()
-time.sleep(2) # try?
-driver.find_element('xpath', '//*[@id="ext-gen9"]').click() #click menu
-time.sleep(1)
-driver.find_element('xpath', '//*[@id="ext-gen93"]').click() #click atendimento
-time.sleep(1)
-driver.find_element('xpath', '//*[@id="ext-gen148"]').click() #click Controle de Agravos
-time.sleep(2)
-
-#driver.switch_to.frame('frame_div_110556') #selecionar frame 'Manutenção Controle de agravos" 
 
 
-os.startfile("exportacoes_teste", "open")
-
-for agravo in CID_AGRAVOS:
-    driver.switch_to.frame('frame_div_110556')
-    time.sleep(2)
-    Select(driver.find_element(By.XPATH, '//*[@id="vSDCONTROLEAGRAVOSSITUACAO"]')).select_by_visible_text("Todos")
-#driver.find_element('xpath', '//*[@id="vCIDAJAX"]').send_keys("A90")
-    driver.find_element('id', 'vCIDAJAX').send_keys(agravo)
-    time.sleep(2)
-    driver.find_element('xpath', '/html/body/div[3]').click()
-    time.sleep(2)
-    driver.find_element('id', 'SEARCHBUTTON').click()
-    time.sleep(2)
-    driver.find_element('xpath', '//*[@id="EXPORT"]').click()
-    time.sleep(2)
-    driver.find_element('id', 'CLEARBUTTON').click()   
-    driver.switch_to.default_content()
-
-
->>>>>>> Stashed changes
